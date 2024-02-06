6651daab27a099b7f9e33c8d8d5734b01dc4f42e
Zavier Hopson <zo.hopson94@gmail.com>
2024-02-06 14:06:53 -0600

    css update

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 // Import functions from section and guest
-import { createSectionHTML } from "./section.js"
-import { createGuestHTML } from "./guest.js"
+import { createSectionHTML } from "./section.js";
+import { createGuestHTML } from "./guest.js";
 
-const sectionHTML = createSectionHTML()
-const guestHTML = createGuestHTML()
+const sectionHTML = createSectionHTML();
+const guestHTML = createGuestHTML();
 
-const parentElement = document.querySelector("#container")
+const parentElement = document.querySelector("#container");
 
 const html = `
     <section>${sectionHTML}</section>
-    <aside>${guestHTML}</aside>
-`
+    <div>${guestHTML}</div>
+`;
 
-parentElement.innerHTML = html
+parentElement.innerHTML = html;
 
 // Declare variable, HTML, and assign it to the following:
-    /*
+/*
     Wrap sectionHTML in section
     Wrap guestHTML in aside (class="guests")
     */
