9e46ea6646ecb6142cc2a040265f6881dd07a147
Zavier Hopson <zo.hopson94@gmail.com>
2024-02-07 11:31:56 -0600

    Updated & corrected css and secion.js

--- scripts/section.js
+++ scripts/section.js
@@ -1,67 +1,33 @@
-<<<<<<< HEAD
-import { getArea, parkAreaService, getService } from "./database.js";
+import { getArea, parkAreaService, getService, getGuest } from "./database.js";
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  if (itemClicked.dataset.type === "area") {
+    let guests = getGuest();
+    let counter = 0;
+    for (const guest of guests) {
+      if (guest.areaID === parseInt(itemClicked.dataset.id)) {
+        counter++;
+      }
+    }
+    window.alert(`There are ${counter} guests in this area.`);
+  }
+});
 
 export const createSectionHTML = () => {
-  let sectionHTML = `<section class="areas">`;
+  let sectionHTML = ``;
   let areas = getArea();
   let services = parkAreaService();
   let tacos = getService();
 
   for (const area of areas) {
     let selectedServices = [];
-    sectionHTML += `<div class="areaWrapper"><u>${area.attraction}:</u> <br>`;
+    sectionHTML += `<section class="areaWrapper" data-type="area" data-id="${area.id}"> ${area.attraction} <br>`;
     sectionHTML += `<ul class="serviceList">`;
     for (const service of services) {
       if (service.parkID === area.id) {
         selectedServices.push(service.serviceID);
       }
-=======
-import { getArea, parkAreaService, getService, getGuest } from "./database.js"
-
-
-document.addEventListener(
-    "click",
-    (clickEvent) => {
-        const itemClicked = clickEvent.target
-        if (itemClicked.dataset.type === "area") {
-            let guests = getGuest()
-            let counter = 0
-            for (const guest of guests) {
-                if (guest.areaID === parseInt(itemClicked.dataset.id)) {
-                    counter ++
-                }
-            }
-            window.alert(`There are ${counter} guests in this area.`)
-        }
-    }
-)
-
-
-
-export const createSectionHTML = () => {
-    let sectionHTML = ``
-    let areas = getArea()
-    let services = parkAreaService()
-    let tacos = getService()
-
-    for (const area of areas) {
-        let selectedServices = []
-        sectionHTML += `<section class="areaWrapper" data-type="area" data-id="${area.id}"> ${area.attraction} <br>`
-        sectionHTML += `<ul class="serviceList">`
-        for (const service of services) {
-            if (service.parkID === area.id) {
-                selectedServices.push(service.serviceID)
-            }
-        }
-        for (const selectedService of selectedServices) {
-            for (const taco of tacos){
-                if (selectedService === taco.id) {
-                    sectionHTML += `<li class="serviceListItem">${taco.name}</li>`
-                }
-            }
-        }
-        sectionHTML += `</ul>`
->>>>>>> main
     }
     for (const selectedService of selectedServices) {
       for (const taco of tacos) {
@@ -71,8 +37,8 @@ export const createSectionHTML = () => {
       }
     }
     sectionHTML += `</ul>`;
-    sectionHTML += `</div>`;
+    sectionHTML += `</section>`;
   }
-  sectionHTML += `</section>`;
+
   return sectionHTML;
 };
