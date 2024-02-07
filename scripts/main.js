8bdca30a371a2aa81bc9d3d0070091a91016d962
Zavier Hopson <zo.hopson94@gmail.com>
2024-02-07 10:20:29 -0600

    css styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 // Import functions from section and guest
-import { createSectionHTML } from "./section.js"
-import { createGuestHTML } from "./guest.js"
+import { createSectionHTML } from "./section.js";
+import { createGuestHTML } from "./guest.js";
 
-const sectionHTML = createSectionHTML()
-const guestHTML = createGuestHTML()
+const sectionHTML = createSectionHTML();
+const guestHTML = createGuestHTML();
 
-const parentElement = document.querySelector("#container")
+const parentElement = document.querySelector("#container");
 
 const html = `
-    <section>${sectionHTML}</section>
-    <aside>${guestHTML}</aside>
-`
+    <section class="areas">${sectionHTML}</section>
+    <aside class="guests">${guestHTML}</aside>
+`;
 
-parentElement.innerHTML = html
+parentElement.innerHTML = html;
 
 // Declare variable, HTML, and assign it to the following:
-    /*
+/*
     Wrap sectionHTML in section
     Wrap guestHTML in aside (class="guests")
     */
--- scripts/section.js
+++ scripts/section.js
@@ -1,31 +1,30 @@
-import { getArea, parkAreaService, getService } from "./database.js"
-
-
+import { getArea, parkAreaService, getService } from "./database.js";
 
 export const createSectionHTML = () => {
-    let sectionHTML = `<section class="areawrapper">`
-    let areas = getArea()
-    let services = parkAreaService()
-    let tacos = getService()
+  let sectionHTML = `<section class="areas">`;
+  let areas = getArea();
+  let services = parkAreaService();
+  let tacos = getService();
 
-    for (const area of areas) {
-        let selectedServices = []
-        sectionHTML += `${area.attraction} <br>`
-        sectionHTML += `<ul class="servicelist">`
-        for (const service of services) {
-            if (service.parkID === area.id) {
-                selectedServices.push(service.serviceID)
-            }
-        }
-        for (const selectedService of selectedServices) {
-            for (const taco of tacos){
-                if (selectedService === taco.id) {
-                    sectionHTML += `<li class="servicelistitem">${taco.name}</li>`
-                }
-            }
-        }
-        sectionHTML += `</ul>`
+  for (const area of areas) {
+    let selectedServices = [];
+    sectionHTML += `<div class="areaWrapper"><u>${area.attraction}:</u> <br>`;
+    sectionHTML += `<ul class="serviceList">`;
+    for (const service of services) {
+      if (service.parkID === area.id) {
+        selectedServices.push(service.serviceID);
+      }
     }
-    sectionHTML += `</section>`
-    return sectionHTML
-}
+    for (const selectedService of selectedServices) {
+      for (const taco of tacos) {
+        if (selectedService === taco.id) {
+          sectionHTML += `<li class="serviceListItem">${taco.name}</li>`;
+        }
+      }
+    }
+    sectionHTML += `</ul>`;
+    sectionHTML += `</div>`;
+  }
+  sectionHTML += `</section>`;
+  return sectionHTML;
+};
