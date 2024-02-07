706a01fd88504d2267268d385956224705c78b3e
Ezra Brewer <ezjbrewer@gmail.com>
2024-02-07 11:51:49 -0600

    added services features v3

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 // Import functions from section and guest
 import { createSectionHTML } from "./section.js";
 import { createGuestHTML } from "./guest.js";
+import { createServiceHTML } from "./services.js";
 
 const sectionHTML = createSectionHTML();
 const guestHTML = createGuestHTML();
+const serviceHTML = createServiceHTML();
 
 const parentElement = document.querySelector("#container");
 
 const html = `
+    <section class="services">${serviceHTML}</section>
     <section class="areas">${sectionHTML}</section>
     <aside class="guests">${guestHTML}</aside>
 `;
